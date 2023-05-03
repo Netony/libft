0f5d41c4e1ac43d55fe01ae2e96a218ed0579136
Daemyeong Jeon <dajeon@c5r6s1.42seoul.kr>
2023-05-03 21:10:03 +0900

    atoi 편집중, 오버플로우 언더플로우 조건

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,31 +1,68 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*   ft_atoi_re.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dajeon <dajeon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/29 18:36:37 by dajeon            #+#    #+#             */
-/*   Updated: 2022/12/13 21:54:01 by dajeon           ###   ########.fr       */
+/*   Created: 2023/05/03 18:33:08 by dajeon            #+#    #+#             */
+/*   Updated: 2023/05/03 20:23:59 by dajeon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+static long	ft_strtol(const char *nptr);
+static int	ft_ctoi(int c, int sign);
+static long	ft_check(long nbr, int new);
+static int	ft_isspace(int c);
+static int	ft_isdigit(int c);
+
 int	ft_atoi(const char *nptr)
 {
-	int	n;
-	int	sign;
+	return (ft_strtol(nptr));
+}
 
-	n = 0;
-	sign = 1;
-	while (*nptr == 32 || (9 <= *nptr && *nptr <= 13))
+static long	ft_strtol(const char *nptr)
+{
+	int		sign;
+	long	nbr;
+
+	while (ft_isspace(*nptr))
 		nptr++;
+	sign = 1;
 	if (*nptr == '-' || *nptr == '+')
 	{
-		if (*nptr == '-')
+		if (*(nptr++) == '-')
 			sign = -1;
-		nptr++;
 	}
-	while ('0' <= *nptr && *nptr <= '9')
-		n = n * 10 + (*(nptr++) - '0');
-	return (sign * n);
+	while (ft_isdigit(*nptr) == 0)
+		nbr = ft_check(nbr, ft_ctoi(*(nptr++), sign));
+	return (nbr);
+}
+
+static int	ft_isspace(int c)
+{
+	if (c == 32 || (9 <= c && c <= 13))
+		return (1);
+	else
+		return (0);
+}
+
+static int	ft_ctoi(int c, int sign)
+{
+	return (c - '0');
+}
+
+static int	ft_isdigit(int c)
+{
+	return ('0' <= c && c <= '9');
+}
+
+static long	ft_check(long nbr, int new)
+{
+	if (nbr * 10 > LONG_MAX + new)
+		return (-1);
+	else if (nbr * 10 < LONG_MIN + new)
+		return (0);
+	else 
+		return (nbr * 10 + new);
 }
