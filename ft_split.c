57c0b89d942de104a8fc5fad24ff4241c5bb0332
Daemyeong Jeon <dajeon@c7r3s1.42seoul.kr>
2023-05-27 17:49:52 +0900

    split strict pass

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: dajeon <dajeon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 19:56:59 by dajeon            #+#    #+#             */
-/*   Updated: 2022/12/14 18:29:16 by dajeon           ###   ########.fr       */
+/*   Updated: 2023/05/26 21:49:33 by dajeon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 static char		**ft_salloc(char const *s, char c, size_t pnum);
 static size_t	ft_split_count(char const *s, char c);
+char			**ft_free_strs(char **strs, size_t size);
 
 char	**ft_split(char const *s, char c)
 {
@@ -67,28 +68,39 @@ static size_t	ft_split_count(char const *s, char c)
 
 static char	**ft_salloc(char const *s, char c, size_t pnum)
 {
-	size_t	i;
 	size_t	j;
 	size_t	len;
 	char	**salloc;
 
-	i = 0;
 	j = 0;
 	salloc = (char **)malloc(sizeof(char *) * (pnum + 1));
-	while (s[i] && salloc != NULL)
+	while (*s && salloc != NULL)
 	{
-		if ((i == 0 || s[i - 1] == c) && s[i] != c)
+		if ((j == 0 || *(s - 1) == c) && *s != c)
 		{
 			len = 0;
-			while (s[i] != c && s[i] != '\0')
+			while (*s != c && *s != '\0')
 			{
-				i++;
+				s++;
 				len++;
 			}
-			salloc[j++] = (char *)malloc(sizeof(char) * (len + 1));
+			salloc[j] = (char *)malloc(sizeof(char) * (len + 1));
+			if (salloc[j++] == NULL)
+				return (salloc = ft_free_strs(salloc, j - 1));
 		}
 		else
-			i++;
+			s++;
 	}
 	return (salloc);
+}
+
+char	**ft_free_strs(char **strs, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < size)
+		free(strs[i++]);
+	free(strs);
+	return (NULL);
 }
